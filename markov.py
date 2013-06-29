3307c824d425382ed9e7b3c15a0a16170fb7b8b6
Hackbright Student <user@mini-002.(none)>
2013-06-28 17:43:38 -0700

    Markov with twitter

--- markov.py
+++ markov.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 import sys
 import random
+import string
+import twitter
+
+api = twitter.Api(consumer_key = '1qwziCIuZeYnDgdUcjJbg', 
+                consumer_secret='qIMK2kORwJMLWb9wWGM9PSdvQpMToI5TNAuDQoVw', 
+                access_token_key='1554230311-hOTyc8nthz27gDp7bOeDqwNsU9KNxRs1R3afZR4', 
+                access_token_secret='JKZxjuRo2PIBkczG68F27do4ERTbQSzFwuJZDrW8')
 
 def make_chains(corpus):
     """Takes an input text as a string and returns a dictionary of markov chains."""
@@ -37,25 +44,45 @@ def make_text(chains):
     based off an original text."""
     # Randomly go through through created dictionary
     # Select a key to print - except last key
-    random_key = random.choice(chains.keys())
-    random_value = find_random_value(chains, random_key)
+    key = random.choice(chains.keys())
+    final_string = " ".join(key)
+    # string.upper(final_string[0])
+    # lst = [final_string[0].upper + final_string[1:]]
+    # final_string = " ".join(lst)
+    # print final_string
+
+    while len(final_string) < 130:
+        random_value = find_random_value(chains, key)
+        final_string = combine(final_string, random_value)
+        
+        key = (key[1], random_value)
+
+    return final_string
+
 
     # This only works if the tupule has ONLY two elements
     # Need to update with a loop if tupule has > 2 elements
-    tupule_elements = combine(random_key[0],random_key[1]) 
+    # tupule_elements = combine(random_key[0],random_key[1]) 
     # Select an element from key's value list to print ie. random_value
-    final_string = combine(tupule_elements,random_value)
+    # final_string = combine(tupule_elements,random_value)
     # print tupule_with_value
 
-    new_random_tuple = (random_key[1], random_value)
+    # new_random_tuple = (random_key[1], random_value)
+    # num_characters = 0
+    # for i in range(20):
+    # while num_characters + len(val) < 140:
 
-    for i in range(20):
-        if new_random_tuple in chains:
-            val = find_random_value(chains,new_random_tuple)
-            final_string = combine(final_string,val)
-            new_random_tuple = (new_random_tuple[1], val)
-
-    return final_string
+        # if new_random_tuple in chains:
+            # val = find_random_value(chains,new_random_tuple)
+            # while len(combine(final_string,val)) < 140:
+            #     final_string = combine(final_string,val)
+            #     new_random_tuple = (new_random_tuple[1], val)
+            #     val = find_random_value(chains,new_random_tuple)
+            # while i in final_string <=140:
+            #     continue
+            # else:
+            #     break
+        # return final_string
 
 
 
@@ -71,19 +98,28 @@ def make_text(chains):
     # Set limit to < 140 characters
 
 def main():
-    script, filename1, filename2 = sys.argv
+    script = sys.argv
+    # filename1, filename2 
+    # text1 = open(filename1).read()
+    # text2 = open(filename2).read()
+    # api2 = twitter.Api()
+    text_twitter1 = api.GetUserTimeline(screen_name='@bexcitement')
+    # text1 = open(text_twitter1).read()
+    tweet_texts = []
+    for tweet in text_twitter1:
+        tweet_texts.append(tweet.text)
 
-    text1 = open(filename1).read()
-    text2 = open(filename2).read()
-
-    #convert input text into a string
-    string_text1 = str(text1).split()
-    string_text2 = str(text2).split()
-    input_text = string_text1 + string_text2
+    initial_text = " ".join(tweet_texts)
+    input_text = initial_text.split()
+    print initial_text
+    print input_text
 
     chain_dict = make_chains(input_text)
     random_text = make_text(chain_dict)
-    print random_text
+    status = api.PostUpdate(random_text)
+    # print status.text
+
+
 
 if __name__ == "__main__":
     main()
